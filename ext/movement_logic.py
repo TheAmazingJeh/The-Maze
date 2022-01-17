347494ff1927c60a0d9ca1b4b01cd84cea8b4eed
TheAmazingJeh <joehbooth2004@gmail.com>
2022-01-17 12:48:03 +0000

    ?

--- ext/movement_logic.py
+++ ext/movement_logic.py
@@ -5,25 +5,44 @@ class Movement():
     def __init__(self):
         log = Logger()
         log.log_init("Info","Initialization","Class","Logic")
-        
-    def collect_movement(self,keys_pressed):
-        if len(keys_pressed) > 0:
-            if keys_pressed[PYG.K_UP] or keys_pressed[PYG.K_w]:
-                return (1,0)
-            elif keys_pressed[PYG.K_DOWN] or keys_pressed[PYG.K_s]:
-                return (-1,0)
-            elif keys_pressed[PYG.K_LEFT] or keys_pressed[PYG.K_a]:
-                return (0,-1)
-            elif keys_pressed[PYG.K_RIGHT]  or keys_pressed[PYG.K_d]:
+        self.valid_keys = {
+            "movement":{
+                "num":{
+                    "up":[1073741906,119],
+                    "down":[1073741905,97],
+                    "left":[1073741904,115],
+                    "right":[1073741903,100],
+                    },
+                "keyvar":{
+                    "up":[PYG.K_w],
+                    "down":[PYG.K_s],
+                    "left":[PYG.K_a],
+                    "right":[PYG.K_d],
+                },
+            },
+        }
+
+    def collect_movement(self,key_pressed,key_pressed_value):
+        print(key_pressed)
+        return
+
+
+        if len(key_pressed_value) > 0:
+            if key_pressed in self.valid_keys["movement"]["num"]["up"] and key_pressed_value[PYG.K_w]:
                 return (0,1)
-            else:
-                return None
-        
+            elif key_pressed in self.valid_keys["movement"]["num"]["down"]:
+                return (0,-1)
+            elif key_pressed in self.valid_keys["movement"]["num"]["left"]:
+                return (-1,0)
+            elif key_pressed in self.valid_keys["movement"]["num"]["right"]:
+                return (1,0)
+        return (0,0)
 
     def move_player(self,level_data,direction):
         
         if direction is not None:
-            print(direction)
+            # Do direction things
+            pass
         return {
             "Ignore Event": False,
             "level_data": level_data
